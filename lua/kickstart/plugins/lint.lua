fe21ce4794b7702692b9af78c5adcd9ab18dfb82
ctrites1 <ctrites1@my.bcit.ca>
2025-05-13 23:08:08 -0700

    update with flog

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -15,6 +15,31 @@ return {
         stdin = true,
         parse = lint.linters.phpcs.parse, -- Keep the original parser
       }
+      local eslint = lint.linters.eslint
+      lint.linters.eslint = {
+        cmd = eslint.cmd,
+        args = eslint.args,
+        stdin = eslint.stdin,
+        stream = eslint.stream,
+        ignore_exitcode = true,
+        root_dir = function(params)
+          local root_patterns = {
+            'eslint.config.js',
+            '.eslintrc.js',
+            '.eslintrc.json',
+            '.eslintrc',
+            'package.json',
+          }
+          for _, pattern in ipairs(root_patterns) do
+            local config_file = vim.fn.findfile(pattern, vim.fn.expand '%:p:h' .. ';')
+            if config_file ~= '' then
+              return vim.fn.fnamemodify(config_file, ':h')
+            end
+          end
+
+          return vim.fn.expand '%:p:h'
+        end,
+      }
       lint.linters_by_ft = {
         python = { 'flake8' },
         php = { 'phpcs' },
@@ -72,11 +97,41 @@ return {
       vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
         group = lint_augroup,
         callback = function()
-          -- Only run the linter in buffers that you can modify in order to
-          -- avoid superfluous noise, notably within the handy LSP pop-ups that
-          -- describe the hovered symbol using Markdown.
-          if vim.opt_local.modifiable:get() then
-            lint.try_lint()
+          -- Skip non-modifiable buffers and neo-tree
+          if not vim.opt_local.modifiable:get() or vim.bo.filetype == 'neo-tree' then
+            return
+          end
+
+          -- Wrap everything in a protected call to suppress ALL errors
+          local ok = pcall(function()
+            local ft = vim.bo.filetype
+            if ft == 'javascript' or ft == 'typescript' or ft == 'javascriptreact' or ft == 'typescriptreact' then
+              -- Check if eslint is executable before trying to lint
+              if vim.fn.executable 'eslint' == 1 then
+                -- Also check for config file
+                local eslint_configs = { 'eslint.config.js', '.eslintrc.js', '.eslintrc.json', '.eslintrc', 'package.json' }
+                local has_config = false
+                for _, config in ipairs(eslint_configs) do
+                  if vim.fn.findfile(config, vim.fn.expand '%:p:h' .. ';') ~= '' then
+                    has_config = true
+                    break
+                  end
+                end
+
+                if has_config then
+                  lint.try_lint()
+                end
+              end
+            else
+              -- For non-JS files, just run the linter normally
+              lint.try_lint()
+            end
+          end)
+
+          -- Silently ignore any errors - we're not even logging them
+          if not ok then
+            -- You could log here if you want to debug later:
+            -- vim.notify("Linting error suppressed", vim.log.levels.DEBUG)
           end
         end,
       })
